1809117002f0445aa2eadd09c91f0f68a723f6c5
Ibrahima Gaye <igaye4341@gmail.com>
2026-08-06 03:36:47 +0000

    fix(core): remove StrictMode to fix Radix UI portal crash on dialog close

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './router'
@@ -6,9 +5,7 @@ import { AuthProvider } from './components/providers/AuthProvider'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
-  </StrictMode>
+  <AuthProvider>
+    <RouterProvider router={router} />
+  </AuthProvider>
 )
